c1631eb522894a5d56dcf150d81f1854cd77f846
Владимир Белобродский <belobrodsky@MacBook-Pro-Vladimir.local>
2022-01-12 10:15:00 +0300

    First attemp on 220112

---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+/*
+ это пример многострочного комментария
+ тест
+ */
+
+let a=1, b=2, c=3
+//это пример однострочного комментария
+var d:Int
+var greeting = "Hello, playground"
+print(greeting)
+print(143*123)
+//: *markdown комментарии*
+
+//: **это жирный**
+//prin
+greeting = "123"
+d = a*81*b*c
+print("значение  d=\(d)")  
+print(Int.max)
+print(UInt.max)
+//print(Mat=)
+var cc=1.2232; var ee=123.12313;
+cc+=3 //сокращенная запись сс=сс+3
+print(144 % b)  // оператор остатка от деления
+print(Double(c)*cc) //умножить данные разных типов не получилось
+//UIPrintInfo()
+//sin(0)
+let number=1_000_000 // классная запись целах чисел... чтобы наглядно было все видно
+print(number+1)
+var myWallet:Decimal=0
+let incomeAfterOperation:Decimal=0.1
+
+myWallet+=incomeAfterOperation
+print(myWallet)
+myWallet+=incomeAfterOperation
+print(myWallet)
+myWallet+=incomeAfterOperation
+print(myWallet)
+myWallet+=incomeAfterOperation
+print(myWallet)
+myWallet+=incomeAfterOperation
+print(myWallet)
+myWallet+=incomeAfterOperation
+print(myWallet)
