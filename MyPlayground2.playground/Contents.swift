904cb0adf668bfa331761148870992c1cabdc685
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-01-13 10:04:21 +0300

    Study Cortezhes (Touple)

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -12,7 +12,9 @@ var greeting = "Hello, playground"
 print(greeting)
 print(143*123)
 //: *markdown комментарии*
-
+/*
+ только нужно включить editor - show rendered markup
+ */
 //: **это жирный**
 //prin
 greeting = "123"
@@ -24,12 +26,13 @@ print(UInt.max)
 var cc=1.2232; var ee=123.12313;
 cc+=3 //сокращенная запись сс=сс+3
 print(144 % b)  // оператор остатка от деления
-print(Double(c)*cc) //умножить данные разных типов не получилось
+print(Double(c)*cc) //умножить данные разных типов не получилось без насильственного приведения типов
 //UIPrintInfo()
 //sin(0)
 let number=1_000_000 // классная запись целах чисел... чтобы наглядно было все видно
 print(number+1)
 var myWallet:Decimal=0
+
 let incomeAfterOperation:Decimal=0.1
 
 myWallet+=incomeAfterOperation
@@ -47,4 +50,29 @@ print(myWallet)
 
 var myBull=true
 if myBull {print("yes")}
+
+/*
+ 13.01.2022 What is cortezh in swift.
+ */
+
+var myTouple = (X:12,Y:32)
+var myTouple2 = (X:1,Y:1)
+var myResultTouple: (X:Int, Y:Int)
+
+
+//myResultTouple = myTouple + myTouple //НА ДУРОЧКУ НЕ РАБОТАЕТ
+
+myResultTouple.X = myTouple.X + myTouple2.X
+myResultTouple.Y = myTouple.Y + myTouple2.Y
+
+print(String(myResultTouple.X)+" "+String(myResultTouple.Y)) // НО ЭТО СКОРЕЕ ВСЕГО КОЛХОЗ - НУЖНО УМНЕЕ
+print("X=\(myResultTouple.X) Y=\(myResultTouple.Y)") //  Так более изящней
+
+var meJustme = (Name:"Vladimir", Surname:"Belobrodsky", age: 35)
+
+print(meJustme.Name)
+
+
+
+
 
