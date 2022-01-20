7c2cd9cdc3388e0080534e33e173adbfb654b92c
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-01-20 13:44:27 +0300

    220120 Unwrapping values

--- Lesson 3. OptionalType.playground/Contents.swift
+++ Lesson 3. OptionalType.playground/Contents.swift
@@ -16,4 +16,18 @@ if optionalInt != nil
     print("we somethingHave")
 } else { print("we have nothing, i.e. nil") }
 
+var tupleWithOptElements: (Int?, Int) = (nil,100)
 
+tupleWithOptElements.0 = 99
+
+//print(Int(tupleWithOptElements.0)) //It does n't work - i need to us unwrapping sympol!
+print(tupleWithOptElements.0!);
+
+
+//var a = 3 + Int(tupleWithOptElements.0)
+
+tupleWithOptElements.0 = 7
+
+print(tupleWithOptElements.0!)
+tupleWithOptElements.0!+=3;
+print(tupleWithOptElements.0!)
