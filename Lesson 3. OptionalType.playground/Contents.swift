3a1c999c9cd832e0854bcf69d420a9c2b44443fb
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-01-20 14:44:14 +0300

    unwrapwith_

--- Lesson 3. OptionalType.playground/Contents.swift
+++ Lesson 3. OptionalType.playground/Contents.swift
@@ -23,7 +23,6 @@ tupleWithOptElements.0 = 99
 //print(Int(tupleWithOptElements.0)) //It does n't work - i need to us unwrapping sympol!
 print(tupleWithOptElements.0!);
 
-
 //var a = 3 + Int(tupleWithOptElements.0)
 
 tupleWithOptElements.0 = 7
@@ -31,3 +30,15 @@ tupleWithOptElements.0 = 7
 print(tupleWithOptElements.0!)
 tupleWithOptElements.0!+=3;
 print(tupleWithOptElements.0!)
+
+//optional binding
+
+
+var pointX: Int? = 774  //Please try something else, like nil
+var pointY: Int? = 3
+if let X = pointX, let Y = pointY {  //this is optional binding like nil
+    print("sum=\(X+Y)") } else { print("one or more value is nil")}
+
+pointY = 123
+if let _ = pointX, let _ = pointY {  //symbol "_" used when variable is not necessery in the code
+    print("sum=\(pointX!+pointY!)") } else { print("one or more value is nil")}
